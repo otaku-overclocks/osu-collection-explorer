9582b8092b306c5f6194dcad07d685d6fa4c96e3
Jérémy <blackrockshooter76@outlook.fr>
2017-01-14 14:05:01 +0100

    Slightly changed preference file so that a folder picker returns usable result

--- Preferences.cs
+++ Preferences.cs
@@ -8,10 +8,10 @@ namespace osu_collection_manager
 {
     public class Preferences
     {
-        //public static string OsuInstallationPath = $@"C:\Users\{Environment.UserName}\AppData\Local\osu!\";
-        public static string OsuInstallationPath = $@"G:\Games\OsuTest\";
+        //public static string OsuInstallationPath = $@"C:\Users\{Environment.UserName}\AppData\Local\osu!";
+        public static string OsuInstallationPath = $@"G:\Games\OsuTest";
 
-        public static string CollectionsDBPath { get { return OsuInstallationPath + "collection.db"; } }
-        public static string OsuDBPath { get { return OsuInstallationPath + "osu!.db"; } }
+        public static string CollectionsDBPath { get { return OsuInstallationPath + "\\collection.db"; } }
+        public static string OsuDBPath { get { return OsuInstallationPath + "\\osu!.db"; } }
     }
 }
