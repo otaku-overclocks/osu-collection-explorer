3f4d49990f2970e2a2a1678721249f4bf6cf710d
EgorDm <egordmitriev2@gmail.com>
2017-01-18 18:01:23 +0100

    Added comments. Its bit more understandable right now.

--- Managers/CollectionManager.cs
+++ Managers/CollectionManager.cs
@@ -15,10 +15,22 @@ namespace osu_collection_manager.Managers
 {
     public class CollectionManager
     {
+        /// <summary>
+        /// Version of osu to properly write the .db file. Some properties are different in various versions.
+        /// Just store it to be safe. We also need to write it back
+        /// </summary>
         private static int _osuVersion;
 
+        /// <summary>
+        /// Only privately settable collections list so we dont overwrite it. 
+        /// If you really need to empty it and write a list to it through addRange.
+        /// </summary>
         private static List<Collection> _collections;
 
+        /// <summary>
+        /// Collections instance. If its null it reads collections.db first.
+        /// If you really need to empty it and write a list to it through addRange.
+        /// </summary>
         public static List<Collection> Collections
         {
             get
@@ -31,60 +43,69 @@ namespace osu_collection_manager.Managers
             }
         }
 
+        /// <summary>
+        /// Reads collection database and writes it to .Collections
+        /// </summary>
+        /// <remarks>
+        /// It throws some exceptions if file is not found, not readable or does not contain the expected data.
+        /// </remarks>  
         private static void ReadCollectionsDB()
         {
+            //Create an empty list
             _collections = new List<Collection>();
-            try
+            //Open collections.db in our custom binary reader
+            using (var r = new CustomReader(File.OpenRead(Preferences.CollectionsDBPath)))
             {
-                using (var r = new CustomReader(File.OpenRead(Preferences.CollectionsDBPath)))
+                //Here we are reading the data
+                //More about it here: https://osu.ppy.sh/wiki/Db_(file_format)#collection.db
+                _osuVersion = r.ReadInt32();
+                var amount = r.ReadInt32();
+                for (var i = 0; i < amount; i++)
                 {
-                    _osuVersion = r.ReadInt32();
-                    var amount = r.ReadInt32();
-                    for (var i = 0; i < amount; i++)
+                    var name = r.ReadString();
+                    var size = r.ReadInt32();
+                    // We dont need the hashes. Instead, we check if osu.db contains some info about them.
+                    // If it does we add a List of beatmaps to the collection model. 
+                    // Note: There are beatmaps from same set through the list. Sorting happens in collections contructor
+                    var beatmaps = new List<Beatmap>(size);
+                    for (var j = 0; j < size; j++)
                     {
-                        var name = r.ReadString();
-                        var size = r.ReadInt32();
-                        var beatmaps = new List<Beatmap>(size);
-                        for (var j = 0; j < size; j++)
-                        {
-                            var map = LocalSongManager.FindByHash(r.ReadString());
-                            if (map != null) beatmaps.Add(new Beatmap(map));
-                        }
-                        _collections.Add(new Collection(name, beatmaps));
+                        var map = LocalSongManager.FindByHash(r.ReadString());
+                        if (map != null) beatmaps.Add(new Beatmap(map));
                     }
+                    _collections.Add(new Collection(name, beatmaps));
                 }
-            }
-            catch (Exception e)
-            {
-                Debug.Fail(e.Message);
             }
         }
 
+        /// <summary>
+        /// Writes .Collections to collections.db
+        /// </summary>
+        /// <remarks>
+        /// It throws some exceptions if directory is not found, not writeable or does not contain the expected data.
+        /// </remarks>  
         public static void WriteCollectionsDB()
         {
-            try
+            //Open collections.db in our custom binary writer
+            using (var w = new CustomWriter(File.Open(Preferences.CollectionsDBPath, FileMode.Create)))
             {
-                using (var w = new CustomWriter(File.Open(Preferences.CollectionsDBPath, FileMode.Create)))
+                //Here we are writing the data
+                //More about it here: https://osu.ppy.sh/wiki/Db_(file_format)#collection.db
+                w.Write(_osuVersion);
+                w.Write(_collections.Count);
+                foreach (var collection in _collections)
                 {
-                    w.Write(_osuVersion);
-                    w.Write(_collections.Count);
-                    foreach (var collection in _collections)
+                    w.Write(collection.Name);
+                    w.Write(collection.MapSets.Count);
+                    // Write hashes from all beatmaps in the hash. Osu does it too
+                    foreach (var mapSet in collection.MapSets)
                     {
-                        w.Write(collection.Name);
-                        w.Write(collection.MapSets.Count);
-                        foreach (var mapSet in collection.MapSets)
+                        foreach (var map in mapSet.Maps)
                         {
-                            foreach (var map in mapSet.Maps)
-                            {
-                                w.Write(map.MD5Hash);
-                            }
+                            w.Write(map.MD5Hash);
                         }
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                Debug.Fail(e.Message);
             }
         }
     }
--- Managers/ImportExportManager.cs
+++ Managers/ImportExportManager.cs
@@ -10,7 +10,7 @@ namespace osu_collection_manager.Managers
     public class ImportExportManager
     {
         /// <summary>
-        /// Check a list of collections if they contain missing mapsets. The missing ones should be downloaded manually.
+        /// Check a list of collections if they contain missing mapsets. The missing ones should be downloaded.
         /// </summary>
         /// <param name="collections"></param>
         /// <returns>List with missing mapsets</returns>
@@ -21,6 +21,7 @@ namespace osu_collection_manager.Managers
             {
                 foreach (var mapSet in collection.MapSets)
                 {
+                    // If no set found in osu.db we add it to the list of not found maps.
                     if (LocalSongManager.FindSetByID(mapSet.SetID) == null)
                         ret.Add(mapSet);
                 }
