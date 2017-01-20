87b2f841c4f55a8d01ca3fa8e537289c7c215223
EgorDm <egordmitriev2@gmail.com>
2017-01-20 22:19:33 +0100

    Added download list style

---
+++ UI/UserControls/Models/MapsetDownloadHolder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using osu_collection_manager.Annotations;
+using OsuMapDownload.Models;
+
+namespace osu_collection_manager.UI.UserControls.Models
+{
+    public class MapsetDownloadHolder : MapSetDownload, INotifyPropertyChanged
+    {
+        //public override float Progress { get; set; }
+
+        public MapsetDownloadHolder(string url, string path, string name = null) : base(url, path, name)
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
