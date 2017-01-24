73b6b6ae2f232bd6e9715305d56cfec09c0d6c7c
EgorDm <egordmitriev2@gmail.com>
2017-01-24 16:22:33 +0100

    Commented some code

--- Managers/DownloadManager.cs
+++ Managers/DownloadManager.cs
@@ -16,29 +16,35 @@ namespace osu_collection_manager.Managers
 
         private static Action _finishCallback { get; set; }
 
+        /// <summary>
+        /// Starts all downloads that are in queue
+        /// </summary>
         private static void UpdateDownload()
         {
+            // Keeps starting the downloads till there a none left in queue or till we reached the max thread count
             while (Queue.Count > 0 && Downloading.Count < Preferences.BloodcatThreadCount)
             {
+                // Get first download in queue
                 var ms = Queue[0];
-                Queue.Remove(ms);
-                Downloading.Add(ms);
-                var task = ms.CreateTask(Preferences.SongsPath);
-                var cont = task.ContinueWith(delegate (Task task1)
+                Queue.Remove(ms); // Remove from queue
+                Downloading.Add(ms); // Add to downloading
+                var task = ms.CreateTask(Preferences.SongsPath); // Create a task to downlaod and extract the mapset
+                var cont = task.ContinueWith(delegate (Task task1) // Create a task which will be run once the downlaod is completed
                 {
-                    Downloading.Remove(ms);
-                    Completed.Add(ms);
-                    UpdateDownload();
-                    if (ms.Failed)
+                    Downloading.Remove(ms); // Remove from downloads
+                    Completed.Add(ms);  // Add to completed
+                    UpdateDownload(); // Start downloading new maps if there are some in queue
+                    if (ms.Failed) // Write t log if dl has failed
                     {
                         LogManager.Open();
                         LogManager.Write($"Failed downloading mapset from url: {ms.Url}");
                         LogManager.Write(ms.Error);
                         LogManager.Close();
                     }
-                }, TaskScheduler.FromCurrentSynchronizationContext());
-                task.Start();
+                }, TaskScheduler.FromCurrentSynchronizationContext()); // Task will be run in ui thread
+                task.Start(); // Start download async
             }
+            // If all downloads are completed and we have set a "all downlaods finished" callback, we will run it and remove it
             if (Queue.Count == 0 && Downloading.Count == 0 && _finishCallback != null)
             {
                 _finishCallback();
@@ -46,6 +52,10 @@ namespace osu_collection_manager.Managers
             }
         }
 
+        /// <summary>
+        /// Starts downloads. Optionally you can specify a callback which will be run after all downloads are finished
+        /// </summary>
+        /// <param name="callback"></param>
         public static void StartDownload(Action callback  = null)
         {
             _finishCallback = callback;
--- Pages/SelectCollectionsPage.xaml.cs
+++ Pages/SelectCollectionsPage.xaml.cs
@@ -34,76 +34,120 @@ namespace osu_collection_manager.Pages
             Export,
             Import
         }
-
+        /// <summary>
+        /// The purpose of the page. Do we want to import or export maps
+        /// </summary>
         public SelectPurpose Purpose;
 
         public SelectCollectionsPage(IEnumerable<Collection> collections, SelectPurpose purpose = SelectPurpose.Export) : base()
         {
             InitializeComponent();
             Purpose = purpose;
+            //We add all the collections into the treelist
             foreach (var collection in collections)
             {
                 CollectionsTreeView.Collections.Add(new CollectionHolder(collection, true));
             }
+            //Set the confirmatiopn button text. Export or Import
             BtnConfirm.Content = Purpose == SelectPurpose.Export ? "Export" : "Import";
         }
 
         private void Confirm_OnClick(object sender, RoutedEventArgs e)
         {
-            var selected = CollectionsTreeView.GetSelected();
+            //Pick what action we want to run with selected beatmaps
             if (Purpose == SelectPurpose.Export)
             {
-                var saveFileDialog = new SaveFileDialog {Filter = "Collections file (*.osc)|*.osc"};
-                if (saveFileDialog.ShowDialog() != true) return;
-                var file = new CollectionsFile(selected);
-                file.WriteToFile(saveFileDialog.FileName);
-                System.Diagnostics.Process.Start("explorer.exe", $"/select,\"{saveFileDialog.FileName}\"");
-                _mainWindow.WindowContent.Content = null;
-                return;
+                Export();
             }
-            //TODO: Split methods
+            else
+            {
+                Import();
+            }
+            
+        }
+
+        private void Export()
+        {
+            //Get selected collections
+            var selected = CollectionsTreeView.GetSelected();
+            //Prompt a dialog to get the path to export to.
+            var saveFileDialog = new SaveFileDialog { Filter = "Collections file (*.osc)|*.osc" };
+            if (saveFileDialog.ShowDialog() != true) return; // Action is cancelled
+            //Put our collections in our file model
+            var file = new CollectionsFile(selected);
+            //Serialize and write the file to our selected path
+            file.WriteToFile(saveFileDialog.FileName);
+            //Select the file in explorer
+            System.Diagnostics.Process.Start("explorer.exe", $"/select,\"{saveFileDialog.FileName}\"");
+            //Go back to main page
+            _mainWindow.WindowContent.Content = null;
+        }
+
+        private void Import()
+        {
+            //Get selected collections
+            var selected = CollectionsTreeView.GetSelected();
+            //Check if some maps are missing
             var missing = ImportExportManager.CheckMissingMapSets(selected);
             if (missing.Count > 0)
             {
+                //Add all missing mapsets to a list with checkboxes where we can pick which we want to download
                 foreach (var missingMapSet in missing)
                 {
                     MapsetList.Mapsets.Add(new MapsetHolder(null, missingMapSet, true));
                 }
+                //Show the "some maps are missing" dialog
                 MissingOverlay.Visibility = Visibility.Visible;
                 DarkenOverlay.Visibility = Visibility.Visible;
                 return;
             }
+            //Import maps if none are missing. We dont need to download anything
             ImportMaps(selected);
         }
 
+        /// <summary>
+        /// Run once the maps we want to download are selected and downlaod button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BtnDownload_OnClick(object sender, RoutedEventArgs e)
         {
-            MissingOverlay.Visibility = Visibility.Hidden;
-            DownloadOverlay.Visibility = Visibility.Visible;
+            MissingOverlay.Visibility = Visibility.Hidden; // Hide missing overlay 
+            DownloadOverlay.Visibility = Visibility.Visible; // Show downlaod overlay
+            //Add all maps to download queue
             foreach (var mapSet in MapsetList.GetSelected())
             {
                 var dl = new MapsetDownloadHolder(mapSet, mapSet.GetBloodcatLink(), Preferences.DownloadsPath);
-                DownloadManager.Queue.Add(dl);
-                DownloadList.Downloads.Add(dl);
+                DownloadManager.Queue.Add(dl); // Add to queue
+                DownloadList.Downloads.Add(dl); // Add to download list (ui)
             }
+            //Start downloads; And add a callback once done
             DownloadManager.StartDownload(() =>
             {
+                // Process the maps. This gets all the beatmaps in folder, 
+                // gets their md5 hash an puts them into Mapset model
                 ImportExportManager.ProcessDownloads(DownloadList.Downloads);
+                // Imports all the maps
                 ImportMaps(CollectionsTreeView.GetSelected());
+                // Hide all overlays
                 DownloadOverlay.Visibility = Visibility.Hidden;
                 DarkenOverlay.Visibility = Visibility.Hidden;
             });
         }
         private void BtnDownloadCancel_OnClick(object sender, RoutedEventArgs e)
         {
+            // Closes the missing maps dialog
             MissingOverlay.Visibility = Visibility.Hidden;
             DarkenOverlay.Visibility = Visibility.Hidden;
         }
 
         private void ImportMaps(IEnumerable<Collection> collections)
         {
+            // Merges current collections with the collections we want to add
             CollectionManager.AddCollections(collections);
+            // Write the database
             CollectionManager.WriteCollectionsDB();
+            // Go back to main menu
             _mainWindow.WindowContent.Content = null;
             MessageBox.Show(_mainWindow, "Maps are imported");
         }
