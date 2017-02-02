55e9e317e2cb5aa6409fd8064f4255ccb5a098f0
EgorDm <egordmitriev2@gmail.com>
2017-02-02 23:53:58 +0100

    Added Window for V0.1 release

---
+++ UI/Windows/CEMainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Windows;
+using osu_collection_manager.UI.Pages;
+
+namespace osu_collection_manager.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for CEMainWindow.xaml
+    /// </summary>
+    public partial class CEMainWindow : BaseNavigationWindow
+    {
+        public CEMainWindow()
+        {
+            InitializeComponent();
+            OpenPage(new MainMenuPage());
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Maximize(object sender, RoutedEventArgs e)
+        {
+            if (WindowState.Equals(WindowState.Maximized))
+            {
+                WindowState = WindowState.Normal;
+                return;
+            }
+            WindowState = WindowState.Maximized;
+        }
+
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        public override void OpenPage(BasePage page)
+        {
+            WindowContent.Content = page;
+        }
+    }
+}
