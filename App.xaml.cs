c1b4c42c73251ddf232e0bd892bf34f335ad0cc4
EgorDm <egordmitriev2@gmail.com>
2017-02-03 21:11:19 +0100

    Check

--- App.xaml.cs
+++ App.xaml.cs
@@ -23,10 +23,10 @@ namespace osu_collection_manager
             {
                 arglist += arg + "\n";
             }
-            if (arglist == "")
+           /* if (arglist == "")
                 MessageBox.Show("No args passed");
             else
-                MessageBox.Show(arglist);
+                MessageBox.Show(arglist);*/
             var path = Preferences.OsuPath;
 #if DEBUG
             Debug.WriteLine($"Found osu path: {path}");
